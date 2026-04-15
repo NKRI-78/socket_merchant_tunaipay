455a0528f9741f81ff2fcdab8d0fa2379db28783
reihan agam <reihanagam7@gmail.com>
2026-04-15 15:58:29 +0700

    update

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.post('/api/v1/payment/qris/webhook', async (req, res) => {
 
     await setTransactionStatus({
       reference_id,
-      status: incomingStatus,
+      status: body.status == '1' ? 'UNPAID' : 'PAID',
       provider_response: JSON.stringify(body),
     });
 
